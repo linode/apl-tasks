5ae5242ef04bce3a3078a7008867f842eec9e9ac
Alin Spinu <spinualin@gmail.com>
2020-09-18 16:01:44 +0300

    refactor: cleanup files

--- bin/aliases.sh
+++ bin/aliases.sh
@@ -3,6 +3,7 @@ alias k="kubectl"
 alias ksk="k -n kube-system"
 alias ki="k -n ingress"
 alias kh="k -n harbor"
+alias kk="k -n keycloak"
 alias kis="k -n istio-system"
 alias ks="k -n system"
 alias ksh="k -n shared"
@@ -23,17 +24,4 @@ function h() { helm $@; }
 
 function drun() {
   docker run --rm -v $PWD:/tmp/sh -w /tmp/sh busybox sh -c ". bin/aliases.sh && $@"
-}
-
-function update_openapi_version() {
-  PACKAGE_VERSION=$(cat package.json | grep '"version"' | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
-  sed -ri "s/^(\s*)version\s*:\s(.*)$/\1version: $PACKAGE_VERSION/" src/openapi/api.yaml
-}
-
-function release_otomi() {
-  rm -rf vendors/client/otomi-api/axios >/dev/null
-  npm run build:client:otomi
-  cd vendors/client/otomi-api/axios
-  npm publish
-  cd -
 }
