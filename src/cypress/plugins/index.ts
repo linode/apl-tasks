26459c6b38c2bc3bb7b70e9791fe0f4ef57d799d
Alin Spinu <spinualin@gmail.com>
2020-09-18 14:02:22 +0300

    refactor

--- src/cypress/plugins/index.ts
+++ src/cypress/plugins/index.ts
@@ -1,37 +1,9 @@
 /// <reference path="../support/index.d.ts" />
 
-// import {
-//   cleanEnv,
-//   TENANT_ID,
-//   TENANT_CLIENT_ID,
-//   TENANT_CLIENT_SECRET,
-//   KEYCLOAK_ADMIN_PASSWORD,
-//   KEYCLOAK_ADDRESS,
-//   KEYCLOAK_CLIENT_ID,
-//   KEYCLOAK_CLIENT_SECRET,
-//   TEAM_USER,
-//   TEAM_USER_PASSWORD
-// } from '../../validators'
-
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  console.log(config) // see what all is in here!
-  console.log(Cypress.env()) // see what all is in here!
-
-// const env = cleanEnv({
-//   TENANT_ID,
-//   TENANT_CLIENT_ID,
-//   TENANT_CLIENT_SECRET,
-//   KEYCLOAK_ADMIN_PASSWORD,
-//   KEYCLOAK_ADDRESS,
-//   KEYCLOAK_CLIENT_ID,
-//   KEYCLOAK_CLIENT_SECRET,
-//   TEAM_USER,
-//   TEAM_USER_PASSWORD
-// })
-
 
   // attach to debugger
   on('before:browser:launch', (browser, args) => {
