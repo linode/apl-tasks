bb72d325facb0a3b84521a26bcc3f1ffcac19fd7
Maurice Faber <maurice.faber@redkubes.com>
2021-11-22 19:23:56 +0100

    fix: added missing files

---
+++ src/tasks/otomi/copy-certs.test.ts
@@ -0,0 +1,49 @@
+import { V1ObjectMeta, V1Secret } from '@kubernetes/client-node'
+import { expect } from 'chai'
+import { targetTlsSecretsFilter } from './copy-certs'
+
+const secretDataBla = {
+  secret: 'bla',
+}
+const secretDataDida = {
+  secret: 'dida',
+}
+const secretBla = {
+  ...new V1Secret(),
+  data: secretDataBla,
+  metadata: {
+    ...new V1ObjectMeta(),
+    annotations: {
+      'app.kubernetes.io/managed-by': 'otomi',
+    },
+    name: 'copy-dev-bla',
+  },
+}
+const secretDida = {
+  ...new V1Secret(),
+  data: secretDataDida,
+  metadata: {
+    ...new V1ObjectMeta(),
+    annotations: {
+      'app.kubernetes.io/managed-by': 'otomi',
+    },
+    name: 'copy-demo-dida',
+  },
+}
+const secretNo = {
+  ...new V1Secret(),
+  data: secretDataDida,
+  metadata: {
+    ...new V1ObjectMeta(),
+    name: 'nomatch',
+  },
+}
+
+const targetTlsSecretsFiltered = [secretBla, secretDida]
+const targetTlsSecrets = [secretBla, secretDida, secretNo]
+
+describe('Task: copy certs', () => {
+  it('targetTlsSecretsFilter should filter copied secrets', () => {
+    expect(targetTlsSecrets.filter(targetTlsSecretsFilter)).to.deep.equal(targetTlsSecretsFiltered)
+  })
+})
