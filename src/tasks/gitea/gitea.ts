72a56ca548d09dc74b722eb6ba5d730506a4485e
Ferruh <63190600+ferruhcihan@users.noreply.github.com>
2023-10-20 13:21:29 +0200

    feat: gitea otomi charts repo (#88)
    * test: otomi-charts
    
    * test: otomi charts
    
    * feat: create otomi/charts repo

--- src/tasks/gitea/gitea.ts
+++ src/tasks/gitea/gitea.ts
@@ -143,6 +143,8 @@ export default async function main(): Promise<void> {
 
   // create main org repo: otomi/values
   await upsertRepo(existingTeams, existingRepos, orgApi, repoApi, repoOption)
+  // create otomi/charts repo for auto image updates
+  await upsertRepo(existingTeams, existingRepos, orgApi, repoApi, { ...repoOption, name: 'charts' })
 
   // add repo: otomi/values to the team: otomi-viewer
   await doApiCall(
