d65dd8b411375dd7c75c548deac2a41a24e3520f
Matthew Elderhorst <matthew.elderhorst@redkubes.com>
2023-11-01 17:06:36 +0100

    feat: modified and deleted secrets

--- src/operator/secrets.ts
+++ src/operator/secrets.ts
@@ -1,72 +1,99 @@
-import Operator, { ResourceEvent, ResourceEventType } from '@dot-i/k8s-operator'
+import Operator, { ResourceEventType } from '@dot-i/k8s-operator'
 import { KubernetesObject } from '@dot-i/k8s-operator/node_modules/@kubernetes/client-node/dist'
 import * as k8s from '@kubernetes/client-node'
 import { KubeConfig } from '@kubernetes/client-node'
 
+// added the type property which was missing in the original KubernetesObject
 interface CustomKubernetesObject extends KubernetesObject {
   type: string
 }
 
+async function createNamespacedSecret(metadata: k8s.V1ObjectMeta | undefined) {
+  if (!metadata) return
+  const simpleSecret = new k8s.V1Secret()
+  simpleSecret.metadata = { name: `copy-${metadata?.namespace}-${metadata?.name}`, namespace: 'argocd' }
+  simpleSecret.type = 'kubernetes.io/dockerconfigjson'
+  try {
+    try {
+      simpleSecret.data = (await k8sApi.readNamespacedSecret(metadata.name!, metadata.namespace!)).body.data
+    } catch (error) {
+      console.debug(`Secret '${metadata.name!}' cannot be found in namespace '${metadata.namespace!}'`)
+    }
+    await k8sApi.createNamespacedSecret('argocd', simpleSecret)
+    console.debug(`Secret '${simpleSecret.metadata.name!}' successfully created in namespace '${metadata.namespace!}'`)
+  } catch (err) {
+    console.debug(`Error copying secret: statuscode: ${err.response.body.code} - message: ${err.response.body.message}`)
+  }
+}
+// todo: still need to separate between argocd and istio-system
 const kc = new KubeConfig()
 kc.loadFromDefault()
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api)
-const teamNamespace: string[] = []
-const watchTeamSecrets = async (event: ResourceEvent) => {
-  // console.debug('WATCH TEAMSECRETS', event)
-  const secrets = await k8sApi.listSecretForAllNamespaces()
-  // Filter by secret type TLS, and Dockerconfig
-}
-const watchNamespaces = async (event: ResourceEvent) => {
-  // console.debug('WATCH NAMESPACES', event)
-  if (event.meta.name.includes('team')) {
-    teamNamespace.push(event.meta.namespace!)
-  }
-  const secrets = await k8sApi.listSecretForAllNamespaces()
-}
 export default class MyOperator extends Operator {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   protected async init() {
-    console.debug('Before watch resource')
     await this.watchResource('', 'v1', 'secrets', async (e) => {
       const { object } = e
       const { metadata, type } = object as CustomKubernetesObject
       if (metadata && !metadata.namespace?.startsWith('team-')) return
       if (type !== 'kubernetes.io/dockerconfigjson') return
-      console.debug('------------')
-      console.debug('TEAM SECRETS: ', metadata?.name)
-      console.debug('EVENT-TYPE: ', e.type)
       switch (e.type) {
         case ResourceEventType.Added: {
-          console.debug('ResourceEventType.Added')
+          await createNamespacedSecret(metadata)
+          break
+        }
+        case ResourceEventType.Modified: {
           const simpleSecret = new k8s.V1Secret()
           simpleSecret.metadata = { name: `copy-${metadata?.namespace}-${metadata?.name}`, namespace: 'argocd' }
           simpleSecret.type = 'kubernetes.io/dockerconfigjson'
-          // eslint-disable-next-line no-useless-catch
           try {
-            simpleSecret.data = (await k8sApi.readNamespacedSecret(metadata!.name!, metadata!.namespace!)).body.data
-            await k8sApi.createNamespacedSecret('argocd', simpleSecret)
-            console.debug(simpleSecret.metadata.name)
-          } catch (err) {
-            console.debug('ERROR', err)
-            console.debug(
-              `Secret '${simpleSecret.metadata.name}' already exists in namespace '${simpleSecret.metadata.namespace}'`,
+            const headers = { 'content-type': 'application/strategic-merge-patch+json' }
+            try {
+              simpleSecret.data = (await k8sApi.readNamespacedSecret(metadata!.name!, metadata!.namespace!)).body.data
+            } catch (error) {
+              console.debug(`Secret '${metadata!.name!}' cannot be found in namespace '${metadata!.namespace!}'`)
+            }
+            await k8sApi.patchNamespacedSecret(
+              simpleSecret.metadata.name!,
+              'argocd',
+              simpleSecret,
+              undefined,
+              undefined,
+              undefined,
+              undefined,
+              { headers },
             )
+            console.debug(
+              `Secret '${simpleSecret.metadata.name!}' successfully patched in namespace '${metadata!.namespace!}'`,
+            )
+          } catch (err) {
+            console.debug(
+              `Error patching copied secret: statuscode: ${err.response.body.code} - message: ${err.response.body.message}`,
+            )
+            if (err.response.body.code === 404) {
+              console.debug('Creating one instead')
+              await createNamespacedSecret(metadata)
+            }
           }
-          // do something useful here
           break
         }
-        case ResourceEventType.Modified:
-          // do something useful here
+        case ResourceEventType.Deleted: {
+          try {
+            await k8sApi.deleteNamespacedSecret(`copy-${metadata?.namespace}-${metadata?.name}`, 'argocd')
+            console.debug(
+              `Secret 'copy-${metadata?.namespace}-${metadata?.name}' successfully deleted in namespace '${metadata!
+                .namespace!}'`,
+            )
+          } catch (err) {
+            console.debug(
+              `Error deleting copied secret: statuscode: ${err.response.body.code} - message: ${err.response.body.message}`,
+            )
+          }
           break
-        case ResourceEventType.Deleted:
-          // do something useful here
-          break
+        }
         default:
           break
       }
-    })
-    await this.watchResource('', 'v1', 'namespaces', watchNamespaces)
-    teamNamespace.forEach((namespace) => {
-      this.watchResource('', 'v1', 'secrets', watchTeamSecrets, namespace)
     })
   }
 }
