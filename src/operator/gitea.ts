1fc18e9001d1544605c86824298b9f995cabe7ab
ElderMatt <matthew.elderhorst@hotmail.com>
2023-12-06 16:15:04 +0100

    feat: added gitea group mapping

--- src/operator/gitea.ts
+++ src/operator/gitea.ts
@@ -1,105 +1,68 @@
-import Operator, { ResourceEventType } from '@dot-i/k8s-operator'
-import { KubernetesObject } from '@dot-i/k8s-operator/node_modules/@kubernetes/client-node/dist'
+import Operator from '@dot-i/k8s-operator'
 import * as k8s from '@kubernetes/client-node'
 import { KubeConfig } from '@kubernetes/client-node'
-
-// added the type property which was missing in the original KubernetesObject
-interface CustomKubernetesObject extends KubernetesObject {
-  type: string
-}
-
-async function createNamespacedSecret(
-  metadata: k8s.V1ObjectMeta | undefined,
-  targetNamespace: string,
-  secretType: string,
-) {
-  if (!metadata) return
-  const simpleSecret = new k8s.V1Secret()
-  simpleSecret.metadata = { name: `copy-${metadata?.namespace}-${metadata?.name}`, namespace: targetNamespace }
-  simpleSecret.type = secretType
-  try {
-    try {
-      simpleSecret.data = (await k8sApi.readNamespacedSecret(metadata.name!, metadata.namespace!)).body.data
-    } catch (error) {
-      console.debug(`Secret '${metadata.name!}' cannot be found in namespace '${metadata.namespace!}'`)
-    }
-    await k8sApi.createNamespacedSecret(targetNamespace, simpleSecret)
-    console.debug(`Secret '${simpleSecret.metadata.name!}' successfully created in namespace '${targetNamespace}'`)
-  } catch (err) {
-    // we know 409 indicates that secret already exists, ignore this code because it will only happen during start of the operator
-    if (err.response.body.code === 409) return
-    console.debug(`Error copying secret: statuscode: ${err.response.body.code} - message: ${err.response.body.message}`)
-  }
-}
 
 const kc = new KubeConfig()
 kc.loadFromDefault()
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api)
+
+function buildTeamString(teamNames: any[]): string {
+  if (teamNames === undefined) return '{}'
+  let result = '{'
+
+  for (let i = 0; i < teamNames.length; i++) {
+    const teamName = teamNames[i]
+    const teamObject = `"/${teamName}":{"otomi":["otomi-viewer", "${teamName}"]}`
+
+    if (i === teamNames.length - 1) {
+      // If it's the last team name, don't add a comma
+      result += teamObject
+    } else {
+      result += `${teamObject}, `
+    }
+  }
+
+  result += '}'
+  return result
+}
+
 export default class MyOperator extends Operator {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   protected async init() {
-    await this.watchResource('', 'v1', 'secrets', async (e) => {
+    await this.watchResource('', 'v1', 'namespaces', async (e) => {
       const { object } = e
-      const { metadata, type } = object as CustomKubernetesObject
+      const { metadata } = object
       if (metadata && !metadata.namespace?.startsWith('team-')) return
-      if (type !== 'kubernetes.io/dockerconfigjson' && type !== 'kubernetes.io/tls') return
-      const targetNamespace = type === 'kubernetes.io/dockerconfigjson' ? 'argocd' : 'istio-system'
-      switch (e.type) {
-        case ResourceEventType.Deleted: {
-          try {
-            await k8sApi.deleteNamespacedSecret(`copy-${metadata?.namespace}-${metadata?.name}`, targetNamespace)
-            console.debug(
-              `Secret 'copy-${metadata?.namespace}-${metadata?.name}' successfully deleted in namespace '${targetNamespace}'`,
-            )
-          } catch (err) {
-            console.debug(
-              `Error deleting copied secret: statuscode: ${err.response.body.code} - message: ${err.response.body.message}`,
-            )
-          }
-          break
+      try {
+        const namespaces = await k8sApi.listNamespace()
+        const teamNamespaces = namespaces.body.items
+          .map((namespace) => namespace.metadata?.name)
+          .filter((name) => name && name.startsWith('team-'))
+        console.info('team namespace: ', teamNamespaces)
+        if (teamNamespaces.length > 0) {
+          const giteaPodLabel = 'app=gitea'
+          const giteaPodList = await k8sApi.listNamespacedPod(
+            'gitea',
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            giteaPodLabel,
+          )
+          const giteaPod = giteaPodList.body.items[0]
+          const execCommand = `gitea admin auth update-oauth --id 1 --group-team-map ${buildTeamString(teamNamespaces)}`
+          await k8sApi.connectPostNamespacedPodExec(
+            giteaPod.metadata?.name || 'gitea-0',
+            giteaPod.metadata?.namespace || 'gitea',
+            execCommand,
+            undefined,
+            true,
+            true,
+            true,
+          )
         }
-        case ResourceEventType.Modified: {
-          const simpleSecret = new k8s.V1Secret()
-          simpleSecret.metadata = { name: `copy-${metadata?.namespace}-${metadata?.name}`, namespace: targetNamespace }
-          simpleSecret.type = type
-          try {
-            const headers = { 'content-type': 'application/strategic-merge-patch+json' }
-            try {
-              simpleSecret.data = (await k8sApi.readNamespacedSecret(metadata!.name!, metadata!.namespace!)).body.data
-            } catch (error) {
-              console.debug(`Secret '${metadata!.name!}' cannot be found in namespace '${metadata!.namespace!}'`)
-            }
-            await k8sApi.patchNamespacedSecret(
-              simpleSecret.metadata.name!,
-              targetNamespace,
-              simpleSecret,
-              undefined,
-              undefined,
-              undefined,
-              undefined,
-              { headers },
-            )
-            console.debug(
-              `Secret '${simpleSecret.metadata.name!}' successfully patched in namespace '${targetNamespace}'`,
-            )
-            break
-          } catch (err) {
-            console.debug(
-              `Error patching copied secret: statuscode: ${err.response.body.code} - message: ${err.response.body.message}`,
-            )
-            // we know 404 indicates that a secret does not exist, in this case we recreate a new one because otherwise it will not create a copy
-            if (err.response.body.code !== 404) break
-            console.debug('Recreating a copy of the secret')
-            await createNamespacedSecret(metadata, targetNamespace, type)
-            break
-          }
-        }
-        case ResourceEventType.Added: {
-          await createNamespacedSecret(metadata, targetNamespace, type)
-          break
-        }
-        default:
-          break
+      } catch (error) {
+        console.debug(error)
       }
     })
   }
@@ -120,6 +83,6 @@ async function main(): Promise<void> {
   process.on('SIGTERM', () => exit('SIGTERM')).on('SIGINT', () => exit('SIGINT'))
 }
 
-// if (typeof require !== 'undefined' && require.main === module) {
-//   main()
-// }
+if (typeof require !== 'undefined' && require.main === module) {
+  main()
+}
