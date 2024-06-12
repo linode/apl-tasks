32d2388459dc22362317479e94dc641458dbd9d6
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2024-06-12 23:53:42 +0200

    test: harbor operator

--- src/operator/harbor.ts
+++ src/operator/harbor.ts
@@ -508,7 +508,7 @@ async function ensureTeamBuildPushRobotAccountSecret(namespace: string, projectN
 async function setupHarbor() {
   console.log('harborOperator', harborOperator)
   if (!harborOperator.harborBaseUrl) return
-  const harborHealthUrl = `${harborOperator.harborBaseUrl}/systeminfo`
+  const harborHealthUrl = `${harborOperator.harborBaseUrl}/api/v2.0/systeminfo`
   // harborHealthUrl is an in-cluster http svc, so no multiple external dns confirmations are needed
   await waitTillAvailable(harborHealthUrl, undefined, { confirmations: 1 })
   const bearerAuth = await getBearerToken()
