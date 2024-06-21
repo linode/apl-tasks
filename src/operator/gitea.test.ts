44eeecae86d5abb7d18b812916a76dff92c11ab2
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2024-06-21 10:35:16 +0200

    feat: update gitea test

--- src/operator/gitea.test.ts
+++ src/operator/gitea.test.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import { buildTeamString } from './gitea'
 
 describe('giteaOperator', () => {
-  const teamNames = ['team-demo', 'team-demo2', 'team-demo3']
+  const teamNames = ['demo', 'demo2', 'demo3']
   it('should create a valid group mapping string with all the teams', () => {
     const mappingString = buildTeamString(teamNames)
     expect(mappingString).to.be.equal(
