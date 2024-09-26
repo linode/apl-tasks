6fabe141e4c37c41707ada631d74130fcdd2ac27
ElderMatt <18527012+ElderMatt@users.noreply.github.com>
2024-09-26 16:24:27 +0200

    fix: put in promise

--- src/operator/watcherutilnew.ts
+++ src/operator/watcherutilnew.ts
@@ -251,28 +251,59 @@ export default abstract class Operator {
 
     const startWatch = async (): Promise<void> => {
       try {
-        const req = await watch.watch(
-          uri,
-          {},
-          (phase, obj) =>
-            this.eventQueue.push({
-              event: {
-                meta: ResourceMetaImpl.createWithPlural(plural, obj),
-                object: obj,
-                type: phase as ResourceEventType,
-              },
-              onEvent,
-            }),
-          (err) => {
-            console.log(`inner error: watch on resource ${id} failed: ${this.errorToJson(err)}`)
-            throw err
-          },
-        )
-        this.watchRequests[id] = req
+        const req = await new Promise((resolve, reject) => {
+          watch.watch(
+            uri,
+            {},
+            (phase, obj) =>
+              this.eventQueue.push({
+                event: {
+                  meta: ResourceMetaImpl.createWithPlural(plural, obj),
+                  object: obj,
+                  type: phase as ResourceEventType,
+                },
+                onEvent,
+              }),
+            (err) => {
+              if (err) {
+                console.log(`inner error: watch on resource ${id} failed: ${this.errorToJson(err)}`)
+                reject(err)
+              }
+            },
+          )
+          resolve(req)
+          reject(new Error())
+        })
+        // this.watchRequests[id] = req
       } catch (error) {
-        console.log(`outer error: watch on resource ${id} failed: ${this.errorToJson(error)}`)
+        console.log('error:', error)
         throw error
       }
+
+      // try {
+      //   const req = await watch.watch(
+      //     uri,
+      //     {},
+      //     (phase, obj) =>
+      //       this.eventQueue.push({
+      //         event: {
+      //           meta: ResourceMetaImpl.createWithPlural(plural, obj),
+      //           object: obj,
+      //           type: phase as ResourceEventType,
+      //         },
+      //         onEvent,
+      //       }),
+      //     (err) => {
+      //       if (err) {
+      //         console.log(`inner error: watch on resource ${id} failed: ${this.errorToJson(err)}`)
+      //         reject(err)
+      //       }
+      //     },
+      //   )
+      // } catch (error) {
+      //   console.log(`outer error: watch on resource ${id} failed: ${this.errorToJson(error)}`)
+      //   throw error
+      // }
     }
     try {
       await startWatch()
