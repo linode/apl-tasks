c903328b821a395180431da8aa25e16b32e5f12e
Ferruh <63190600+ferruhcihan@users.noreply.github.com>
2024-10-24 10:14:26 +0200

    fix: platform admin as org owner (#121)
    * fix: assign platform admins as owner
    
    * fix: gitea tests

--- src/tasks/common.ts
+++
@@ -1,5 +0,0 @@
-export const orgName = 'otomi'
-export const teamNameOwners = 'Owners'
-export const teamNameViewer = 'otomi-viewer'
-export const otomiValuesRepoName = 'values'
-export const username = 'otomi-admin'
