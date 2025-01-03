44b654ec6d5b2ea39c764d350f374dc72a089a43
ElderMatt <18527012+ElderMatt@users.noreply.github.com>
2025-01-03 11:53:31 +0100

    fix: group mapping

--- src/operator/gitea.test.ts
+++ src/operator/gitea.test.ts
@@ -6,7 +6,7 @@ describe('giteaOperator', () => {
   it('should create a valid group mapping string with all the teams', () => {
     const mappingString = buildTeamString(teamNames)
     expect(mappingString).to.be.equal(
-      '{"platform-admin":{"otomi":["Owners"]},"team-demo":{"team-demo":["otomi-viewer","team-demo"]},"team-demo2":{"team-demo2":["otomi-viewer","team-demo2"]},"team-demo3":{"team-demo3":["otomi-viewer","team-demo3"]}}',
+      '{"platform-admin":{"otomi":["Owners"]},"team-demo":{"demo":["otomi-viewer","team-demo"]},"team-demo2":{"demo2":["otomi-viewer","team-demo2"]},"team-demo3":{"demo3":["otomi-viewer","team-demo3"]}}',
     )
     expect(mappingString).to.not.contain('team-admin')
   })
