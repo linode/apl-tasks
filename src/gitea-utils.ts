39a5e13281017dbc01599f727961bbdeb4c3ccaa
ElderMatt <18527012+ElderMatt@users.noreply.github.com>
2025-02-17 16:47:23 +0100

    fix: creating secret with annotations

--- src/gitea-utils.ts
+++ src/gitea-utils.ts
@@ -1,5 +1,5 @@
 import { V1Secret } from '@kubernetes/client-node'
-import { createSecret, k8s } from './k8s'
+import { k8s } from './k8s'
 
 // eslint-disable-next-line import/prefer-default-export
 export async function setServiceAccountSecret(
@@ -30,12 +30,25 @@ export async function setServiceAccountSecret(
     if (error.statusCode === 404) {
       console.log(`Secret ${serviceAccountSecretName} could not be found in namespace ${teamNamespace}!`)
       console.log(`Creating secret for ${serviceAccountSecretName} in namespace ${teamNamespace}`)
-      await createSecret(
-        serviceAccountSecretName,
-        teamNamespace,
-        { username: serviceAccountLogin, password },
-        'kubernetes.io/basic-auth',
-      )
+      try {
+        const newSecret: V1Secret = {
+          metadata: {
+            name: 'gitea-credentials',
+            namespace: teamNamespace,
+            annotations: { 'tekton.dev/git-0': giteaUrl },
+          },
+          data: {
+            username: Buffer.from(serviceAccountLogin).toString('base64'),
+            password: Buffer.from(password).toString('base64'),
+          },
+          type: 'kubernetes.io/basic-auth',
+        }
+        await k8s.core().createNamespacedSecret(teamNamespace, newSecret)
+      } catch (creatingError) {
+        console.error(
+          `Problem creating secret ${serviceAccountSecretName} in namespace ${teamNamespace}: ${creatingError}`,
+        )
+      }
     }
     console.error(`Problem replacing secret ${serviceAccountSecretName} in namespace ${teamNamespace}`)
   }
