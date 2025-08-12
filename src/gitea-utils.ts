cad172291158cfa88fb0bb8d4d42afb21d8a1255
Cas Lubbers <clubbers@akamai.com>
2025-08-12 15:08:33 +0200

    feat: test 3

--- src/gitea-utils.ts
+++ src/gitea-utils.ts
@@ -50,9 +50,8 @@ export async function setServiceAccountSecret(
           `Problem creating secret ${serviceAccountSecretName} in namespace ${teamNamespace}: ${creatingError}`,
         )
       }
-    } else {
-      console.error(`Problem replacing secret ${serviceAccountSecretName} in namespace ${teamNamespace}: ${error}`)
     }
+    console.error(`Problem replacing secret ${serviceAccountSecretName} in namespace ${teamNamespace}: ${error}`)
   }
   return password
 }
