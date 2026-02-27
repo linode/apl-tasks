08ffe7b4f1e0cc3c11731aec095da2fb7378d1fd
ElderMatt <18527012+ElderMatt@users.noreply.github.com>
2026-02-27 10:55:15 +0100

    chore: interfaces and consts to new file (#313)
    * chore: interface and consts to separate files
    
    * chore: more consts to separate file
    
    * chore: consts with uppercase

---
+++ src/operators/harbor/lib/types/robot.ts
@@ -0,0 +1,26 @@
+// Interfaces
+export interface RobotSecret {
+  id: number
+  name: string
+  secret: string
+}
+
+export interface RobotAccess {
+  resource: string
+  action: string
+}
+
+export interface RobotPermission {
+  kind: 'project' | 'system'
+  namespace: string
+  access: RobotAccess[]
+}
+
+export interface RobotAccount {
+  name: string
+  duration: number
+  description: string
+  disable: boolean
+  level: 'project' | 'system'
+  permissions: RobotPermission[]
+}
